5d41e978687b7ea6f90cd53c1af5daee77a84377
maanasanarayan <mnsnryn@gmail.com>
2021-12-31 17:54:04 -0500

    Created server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Hello Express! :)")
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port: ${port}`);
+})
