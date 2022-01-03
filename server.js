ab14a49066fd33571a0893a1938c4695a87f4705
maanasanarayan <mnsnryn@gmail.com>
2022-01-03 00:22:48 -0500

    Rendering dynamic content via ejs

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-const express = require("express");
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.get("/", (request, response) => {
-    response.send("Hello Express! :)")
+// Setting the template engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Setting a middleware - to let express search this folder to find any request for styling
+app.use(express.static(path.join(__dirname, './static')));
+
+// Getting routes
+app.get('/', (request, response) => {
+  // response.send("Hello Express! :)");
+
+  // Loading static content
+  // response.sendFile(path.join(__dirname, './static/index.html'));
+
+  // Data for dynamic content in ejs
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
+app.get('/speakers', (request, response) => {
+  // response.send("Hello Express! :)");
+  response.sendFile(path.join(__dirname, './static/speakers.html'));
+});
+
+// Listening to the port
 app.listen(port, () => {
-    console.log(`Listening to port: ${port}`);
-})
+  console.log(`Listening to port: ${port}`);
+});
